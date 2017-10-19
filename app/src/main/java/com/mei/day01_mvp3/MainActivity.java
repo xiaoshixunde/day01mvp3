ff95680833aaf2acae385594e791a2d8a67a99fc
Administrator <1486116063@qq.com>
2017-10-19 19:39:59 +0800

    第二次

---
+++ app/src/main/java/com/mei/day01_mvp3/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mei.day01_mvp3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvxe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+    }
+
+    private void initView() {
+        tvxe = (TextView) findViewById(R.id.tvxe);
+    }
+}
